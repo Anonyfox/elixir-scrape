d17d7b0084d5c8b7dc0b54c3e1138cb03b35f86a
Maximilian Stroh <Hisako1337@gmail.com>
2015-11-04 19:13:52 +0100

    iso encoded pages work now

--- lib/scrape.ex
+++ lib/scrape.ex
@@ -2,14 +2,14 @@ defmodule Scrape do
   alias Scrape.Fetch
   alias Scrape.Website
   alias Scrape.Feed
-  
-  def website(url \\ "http://www.n24.de/n24/Nachrichten/Politik/d/6792546/bundestags-it-muss-sich-offenbar-ergeben.html") do
+
+  def website(url) do
     url
     |> Fetch.run
     |> Website.parse(url)
   end
 
-  def feed(url \\ "http://www.n-tv.de/rss") do 
+  def feed(url) do 
     url
     |> Fetch.run
     |> Feed.parse
--- lib/scrape/fetch.ex
+++ lib/scrape/fetch.ex
@@ -1,16 +1,34 @@
 defmodule Scrape.Fetch do
-  use HTTPoison.Base
 
   # http://www.erlang.org/doc/apps/stdlib/unicode_usage.html
   # http://www.erlang.org/doc/man/unicode.html
   # http://www.reddit.com/r/elixir/comments/39azpw/character_encoding_conversion_in_elixir/
   def run(url) do
-    HTTPoison.get!(url).body
-    # ToDo: binary to utf8 string conversion
-    # if String.valid?(response)
-    #   return response
-    # else
-    #   transcode response and return
-    # end
+    response = HTTPoison.get! url
+    cs = charset(response.headers)
+    if cs do
+      encoding = cs
+      |> String.replace("-","_")
+      |> String.downcase
+      |> to_char_list
+      |> List.to_atom
+      Codepagex.to_string response.body, encoding
+    else
+      response.body
+    end
+  end
+
+  defp charset(headers) do
+    header = headers
+    |> Enum.filter(fn({k,v}) -> k == "Content-Type" end)
+    |> List.first
+    if header do
+      {name, content} = header
+      ~r/charset=(ISO-8859-[1-9])/i
+      |> Regex.run(content, capture: :all_but_first)
+      |> List.first
+    else
+      nil
+    end
   end
 end
