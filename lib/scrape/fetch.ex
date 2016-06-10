f6521083e51c30b1033ae42fdbce69ffa825bcd7
Jovanny Cruz <jovannypcg@yahoo.com>
2016-06-10 10:46:31 -0500

    Improve the code style in the Scrape.Fetch module

--- lib/scrape/fetch.ex
+++ lib/scrape/fetch.ex
@@ -1,5 +1,4 @@
 defmodule Scrape.Fetch do
-
   @opts [
     follow_redirect: true,
     timeout: 33_000,
@@ -15,13 +14,16 @@ defmodule Scrape.Fetch do
   defp evaluate({:error, _}), do: ""
   defp evaluate({:ok, response}) do
     cs = charset(response.headers)
+
     if cs do
       encoding = cs
       |> String.replace("-","_")
       |> String.downcase
       |> to_char_list
       |> List.to_atom
+
       {_status, result} = Codepagex.to_string response.body, encoding
+
       result
     else
       response.body
@@ -32,8 +34,10 @@ defmodule Scrape.Fetch do
     header = headers
     |> Enum.filter(fn({k,_}) -> k == "Content-Type" end)
     |> first
+
     if header do
       {_name, content} = header
+
       ~r/charset=(ISO-8859-[1-9])/i
       |> Regex.run(content, capture: :all_but_first)
       |> first
