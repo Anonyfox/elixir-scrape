ca73c388111fcfc81870d25556767ff6a072b55f
RaphSfeir <raph.sfeir@gmail.com>
2018-11-30 15:03:11 +0100

    Add some documentation, and FeedItem structure, update Timex for tests to work again.

--- lib/scrape.ex
+++ lib/scrape.ex
@@ -1,8 +1,27 @@
 defmodule Scrape do
   alias Scrape.Fetch
   alias Scrape.Feed
+  alias Scrape.FeedItem
   alias Scrape.Website
   alias Scrape.Article
+
+
+  @moduledoc """
+  Elixir package to scrape websites. Current features:
+    can handle non-utf-8 sources.
+    can deal with timezones.
+    parse RSS/Atom feeds.
+    parse common websites.
+    parse advanced content websites ("articles").
+  """
+
+  @doc """
+  Parse an Atom / RSS Feed.
+  Returns a list of items with meta data of each Feed Item, or
+  An empty list if something went wrong or Feed is empty
+  """
+
+  @spec feed(String.t, Atom) :: [%FeedItem{}] | []
 
   def feed(url, :minimal) do
     url
@@ -11,18 +30,33 @@ defmodule Scrape do
     |> Feed.parse_minimal
   end
 
+  @spec feed(String.t) :: [%FeedItem{}] | []
+
   def feed(url) do
     url
     |> Fetch.run
     |> decode_if_needed()
     |> Feed.parse(url)
   end
+  @doc """
+  Parse a website and gets it's various meta data including description, text content, image, tags, favicon and feeds.
+  Returns Map with structured meta data.
+  """
+
+  @spec website(String.t) :: %Website{}
 
   def website(url) do
     url
     |> Fetch.run
     |> Website.parse(url)
   end
+
+  @doc """
+  Parse an article (website with content) and gets it's various meta data.
+  Returns Map with structured meta data (same as website with fulltext field added).
+  """
+
+  @spec article(String.t) :: %Article{}
 
   def article(url) do
     html = Fetch.run url
---
+++ lib/scrape/feed_item.ex
@@ -0,0 +1,3 @@
+defmodule Scrape.FeedItem do
+  defstruct title: "", description: "", url: "", author: "", tags: [], image: "", pubdate: ""
+end
