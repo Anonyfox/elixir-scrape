21a8f0b6ac662e8b0f73dac0015c659ccd6d265e
Maximilian Stroh <Hisako1337@gmail.com>
2019-05-02 23:43:05 +0200

    small improvement

--- lib/scrape/flow/domain.ex
+++ lib/scrape/flow/domain.ex
@@ -17,46 +17,46 @@ defmodule Scrape.Flow.Domain do
     |> finalize()
   end
 
-  defp fetch_html(%__MODULE__{url: url} = data) do
+  defp fetch_html(%{url: url} = data) do
     case HTTP.get(url) do
       {:ok, html} -> %{data | html: html}
       _ -> %{data | halted: true, error: :http_error}
     end
   end
 
-  defp parse_html(%__MODULE__{halted: true} = data), do: data
+  defp parse_html(%{halted: true} = data), do: data
 
-  defp parse_html(%__MODULE__{html: html} = data) do
+  defp parse_html(%{html: html} = data) do
     %{data | dom: Floki.parse(html)}
   end
 
-  defp extract_title(%__MODULE__{halted: true} = data), do: data
+  defp extract_title(%{halted: true} = data), do: data
 
-  defp extract_title(%__MODULE__{dom: dom} = data) do
+  defp extract_title(%{dom: dom} = data) do
     %{data | title: DOM.title(dom)}
   end
 
-  defp extract_description(%__MODULE__{halted: true} = data), do: data
+  defp extract_description(%{halted: true} = data), do: data
 
-  defp extract_description(%__MODULE__{dom: dom} = data) do
+  defp extract_description(%{dom: dom} = data) do
     %{data | description: DOM.description(dom)}
   end
 
-  defp extract_icon_url(%__MODULE__{halted: true} = data), do: data
+  defp extract_icon_url(%{halted: true} = data), do: data
 
-  defp extract_icon_url(%__MODULE__{dom: dom, url: url} = data) do
+  defp extract_icon_url(%{dom: dom, url: url} = data) do
     %{data | icon_url: DOM.icon_url(dom, url)}
   end
 
-  defp extract_feed_urls(%__MODULE__{halted: true} = data), do: data
+  defp extract_feed_urls(%{halted: true} = data), do: data
 
-  defp extract_feed_urls(%__MODULE__{dom: dom, url: url} = data) do
+  defp extract_feed_urls(%{dom: dom, url: url} = data) do
     %{data | feed_urls: DOM.feed_urls(dom, url)}
   end
 
-  defp finalize(%__MODULE__{halted: true, error: error}), do: {:error, error}
+  defp finalize(%{halted: true, error: error}), do: {:error, error}
 
-  defp finalize(%__MODULE__{} = data) do
+  defp finalize(data) do
     keys = [:url, :title, :description, :icon_url, :feed_urls]
     {:ok, Map.take(data, keys)}
   end
