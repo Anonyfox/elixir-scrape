5a5d2b6d3298cfde3971785cdbe37a1143e209ba
Maximilian Stroh <Hisako1337@gmail.com>
2019-05-06 16:55:25 +0200

    added image_url test

--- lib/scrape/flow/steps/extract_image_url.ex
+++ lib/scrape/flow/steps/extract_image_url.ex
@@ -1,7 +1,23 @@
 defmodule Scrape.Flow.Steps.ExtractImageURL do
   @moduledoc false
 
+  def execute(state) when not is_map(state) do
+    {:error, :no_state_given}
+  end
+
+  def execute(%{dom: dom}) when not is_list(dom) and not is_tuple(dom) do
+    {:error, :dom_invalid}
+  end
+
   def execute(%{dom: dom, url: url}) do
     {:ok, %{image_url: Scrape.IR.DOM.image_url(dom, url)}}
+  end
+
+  def execute(%{dom: dom}) do
+    {:ok, %{image_url: Scrape.IR.DOM.image_url(dom)}}
+  end
+
+  def execute(_map) do
+    {:error, :dom_missing}
   end
 end
---
+++ test/flow/steps/extract_image_url_test.exs
@@ -0,0 +1,45 @@
+defmodule Scrape.Flow.Steps.ExtractImageURLTest do
+  use ExUnit.Case
+
+  alias Scrape.Flow.Steps.ExtractImageURL
+
+  test "refuses if no state is given" do
+    assert ExtractImageURL.execute(nil) == {:error, :no_state_given}
+  end
+
+  test "refuses if dom not existing in state" do
+    assert ExtractImageURL.execute(%{}) == {:error, :dom_missing}
+  end
+
+  test "refuses if dom is nil" do
+    assert ExtractImageURL.execute(%{dom: nil}) == {:error, :dom_invalid}
+  end
+
+  test "refuses if dom is not parsed" do
+    assert ExtractImageURL.execute(%{dom: ""}) == {:error, :dom_invalid}
+  end
+
+  test "works if non-empty html string is given" do
+    dom = Floki.parse("<html><meta property='og:image' content='/img.jpg' /></html>")
+    assert ExtractImageURL.execute(%{dom: dom}) == {:ok, %{image_url: "/img.jpg"}}
+  end
+
+  test "does not normalize url if root_url is nil" do
+    dom = Floki.parse("<html><meta property='og:image' content='/img.jpg' /></html>")
+    url = nil
+    {:ok, data} = ExtractImageURL.execute(%{dom: dom, url: url})
+    assert data.image_url == "/img.jpg"
+  end
+
+  test "normalizes url if root_url is given" do
+    dom = Floki.parse("<html><meta property='og:image' content='/img.jpg' /></html>")
+    url = "http://example.com"
+    {:ok, data} = ExtractImageURL.execute(%{dom: dom, url: url})
+    assert data.image_url == "http://example.com/img.jpg"
+  end
+
+  test "transforms empty ImageURL to nil if nothing is found" do
+    dom = Floki.parse("<html><body /></html>")
+    assert ExtractImageURL.execute(%{dom: dom}) == {:ok, %{image_url: nil}}
+  end
+end
