46429d3a5867c92774bbcb868175615b94dce460
Maximilian Stroh <Hisako1337@gmail.com>
2019-05-06 17:14:04 +0200

    added feed_urls test

---
+++ test/flow/steps/extract_feed_urls_test.exs
@@ -0,0 +1,40 @@
+defmodule Scrape.Flow.Steps.ExtractFeedURLsTest do
+  use ExUnit.Case
+
+  alias Scrape.Flow.Steps.ExtractFeedURLs
+
+  test "refuses if no state is given" do
+    assert ExtractFeedURLs.execute(nil) == {:error, :no_state_given}
+  end
+
+  test "refuses if dom not existing in state" do
+    assert ExtractFeedURLs.execute(%{}) == {:error, :dom_missing}
+  end
+
+  test "refuses if dom is nil" do
+    assert ExtractFeedURLs.execute(%{dom: nil}) == {:error, :dom_invalid}
+  end
+
+  test "refuses if dom is not parsed" do
+    assert ExtractFeedURLs.execute(%{dom: ""}) == {:error, :dom_invalid}
+  end
+
+  test "works if non-empty html string is given" do
+    dom = Floki.parse("<html><link rel='alternate' href='/feed.rss'></html>")
+    assert ExtractFeedURLs.execute(%{dom: dom}) == {:ok, %{feed_urls: ["/feed.rss"]}}
+  end
+
+  test "does not normalize url if root_url is nil" do
+    dom = Floki.parse("<html><link rel='alternate' href='/feed.rss'></html>")
+    url = nil
+    {:ok, data} = ExtractFeedURLs.execute(%{dom: dom, url: url})
+    assert data.feed_urls == ["/feed.rss"]
+  end
+
+  test "normalizes url if root_url is given" do
+    dom = Floki.parse("<html><link rel='alternate' href='/feed.rss'></html>")
+    url = "http://example.com"
+    {:ok, data} = ExtractFeedURLs.execute(%{dom: dom, url: url})
+    assert data.feed_urls == ["http://example.com/feed.rss"]
+  end
+end
