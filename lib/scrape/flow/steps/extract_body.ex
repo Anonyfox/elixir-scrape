0bb7eb916d61dfaaf4762ac3998a5d6c8d55b3da
Maximilian Stroh <Hisako1337@gmail.com>
2019-05-06 18:14:31 +0200

    added extract_body test

--- lib/scrape/flow/steps/extract_body.ex
+++ lib/scrape/flow/steps/extract_body.ex
@@ -1,7 +1,22 @@
 defmodule Scrape.Flow.Steps.ExtractBody do
   @moduledoc false
 
+  def execute(state) when not is_map(state) do
+    {:error, :no_state_given}
+  end
+
+  def execute(%{html: html}) when not is_binary(html) do
+    {:error, :html_invalid}
+  end
+
   def execute(%{html: html}) do
-    {:ok, %{body: html |> Scrape.IR.DOM.paragraphs() |> Enum.join(".\n\n")}}
+    case Scrape.IR.DOM.paragraphs(html) do
+      [] -> {:ok, %{body: nil}}
+      paragraphs -> {:ok, %{body: paragraphs |> Enum.join(".\n\n")}}
+    end
+  end
+
+  def execute(_state) do
+    {:error, :html_missing}
   end
 end
