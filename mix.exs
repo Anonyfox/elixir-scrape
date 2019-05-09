61efbd480e4edb9bf213af1d5542c335c9500495
Maximilian Stroh <Hisako1337@gmail.com>
2019-05-09 11:53:37 +0200

    dep notes

--- mix.exs
+++ mix.exs
@@ -39,14 +39,21 @@ defmodule Scrape.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
+      # enable development with `mix test.watch --stale`
       {:mix_test_watch, "~> 0.8", only: :dev, runtime: false},
+      # documentation generation
       {:ex_doc, "~> 0.20.2", only: :dev, runtime: false},
+      # language detection
       {:paasaa, "~> 0.3.1"},
+      # snowball stemmer for multiple languages with a NIF
       {:stemex, "~> 0.1.1"},
+      # HTML/XML parser with CSS3 selectors
       {:floki, "~> 0.21.0"},
+      # clone of arc90's readability algorithm
       {:readability, "~> 0.10.0"},
       # iconv written in pure elixir
       {:codepagex, "~> 0.1.4"},
+      # http client
       {:httpoison, "~> 1.5", override: true}
     ]
   end
