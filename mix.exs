28c549f32a905ec0d27cac63fb600f89ae778f89
Anonyfox <hisako1337@gmail.com>
2019-06-05 10:54:58 +0200

    publishbump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Scrape.MixProject do
   def project do
     [
       app: :scrape,
-      version: "3.0.1",
+      version: "3.0.2",
       elixir: "~> 1.8",
       description: description(),
       package: package(),
